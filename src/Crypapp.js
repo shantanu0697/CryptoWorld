37eb950af8a12cc1942e3e4e92bb1e4697e0ca58
Shantanu <shantanu69kulkarni@gmail.com>
2024-05-12 23:40:13 +0530

    first commit

--- src/Crypapp.js
+++ src/Crypapp.js
@@ -1,54 +1,60 @@
-import React, { useEffect, useState } from 'react';
-import './Crypcss.css';
-import axios from 'axios'
-import Coin from './Coin';
-
+import React, { useEffect, useState } from "react";
+import "./Crypcss.css";
+import axios from "axios";
+import Coin from "./Coin";
 
 function Crypapp() {
-  const [coins,setCoins] = useState([])
-  const [search,setSearch] = useState('')
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
+
   useEffect(() => {
-    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR&order=market_cap_desc&per_page=100&page=1&sparkline=false')
-    .then(res=>{
-       setCoins(res.data)
-       console.log(res.data)
-    }).catch(error=>console.log(error))
-  }, [])
+    const apiKey = process.env.REACT_APP_API_KEY;
+    console.log(process.env.REACT_APP_API_KEY);
+
+    axios
+      .get(apiKey)
+      .then((res) => {
+        setCoins(res.data);
+        console.log(res.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
   // search handling
-  const handleChange = e =>{
-    setSearch(e.target.value)
-  }
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
 
-  const filteredCoins = coins.filter(coin=>
+  const filteredCoins = coins.filter((coin) =>
     coin.name.toLowerCase().includes(search.toLowerCase())
-    )
+  );
   return (
     <div className="coin-app">
       <div className="coin-search">
         <h1 className="coin-text">CryptoWorld</h1>
         <form action="">
-          <input type="text" className="coin-input" placeholder="Provide the coin name" onChange={handleChange}/>
-
+          <input
+            type="text"
+            className="coin-input"
+            placeholder="Provide the coin name"
+            onChange={handleChange}
+          />
         </form>
-
       </div>
-      {filteredCoins.map(coin=>{
-        return(
-          <Coin 
-          key={coin.id} 
-          name={coin.name} 
-          image={coin.image} 
-          symbol={coin.symbol}
-          marketcap={coin.market_cap}
-          price={coin.current_price}
-          pricechange={coin.price_change_percentage_24h}
-           volume={coin.total_volume}
+      {filteredCoins.map((coin) => {
+        return (
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            image={coin.image}
+            symbol={coin.symbol}
+            marketcap={coin.market_cap}
+            price={coin.current_price}
+            pricechange={coin.price_change_percentage_24h}
+            volume={coin.total_volume}
           />
         );
       })}
-
-
     </div>
   );
 }
